ae010b7be7b669e682f7512cd157e07d1f9db019
Stacy Holtz <stacyholtz6@gmail.com>
2019-10-13 18:54:15 -0600

    working on loops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,6 @@ var wrongLetters = [];
 var answerArray = [];
 
 
-
 // create variables that reference the html
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
@@ -18,48 +17,67 @@ var wrongLettersText = document.getElementById("wrongLetters-text");
 var letterGuessedText = document.getElementById("letter-guessed-text");
 var answerText = document.getElementById("answer-text");
 
-
-// picks a random word from girlsList
+// picks a random word from words array
 var word = words[Math.floor(Math.random() * words.length)];
-
+var letterLefttoFind = word.length;
+reset()
 // function to run whenever a user presses a key
 document.onkeyup = function (event) {
   // determines which key was pressed
   var userGuess = event.key.toLowerCase();
-  // adds guess to array
-  // wrongLetters.push(" " + userGuess);
+  letterGuessedText.textContent = userGuess;
+  guessesLeft--
+  console.log(guessesLeft)
+  guessesLeftText.textContent = guessesLeft;
+  if (correct(userGuess) == true) {
+    correctUserGuess(word, userGuess)
+    answerText.textContent = answerArray.join(" ");
+  }
+  else {
+    // adds guess to array
+    wrongLetters.push(" " + userGuess);
+    wrongLettersText.textContent = wrongLetters;
+  }
+  if (letterLefttoFind === 0) {
+    alert("you win");
+    word = words[Math.floor(Math.random() * words.length)];
+    reset()
+    wins++
+    winsText.textContent = wins
+  }
+  else if (guessesLeft === 0) {
+    alert("you loose the word was: ", word)
+    lossesText++
+    lossesText.textContent = losses
+  }
 
-  // add to answer array
+}
 
-
+// game reset
+function reset() {
   // shows blank spaces for the random word
   for (var i = 0; i < word.length; i++) {
     answerArray[i] = " _ ";
   }
   var remainingLetters = word.length;
-
   console.log(answerArray.join(" "));
-  console.log("user guess: " + userGuess);
+  // console.log("user guess: " + userGuess);
   console.log("word: " + word);
   console.log("Wrong Letters: " + wrongLetters);
 
-  // put the correct letter guesses in the blank spaces of answerArray
-
-  // put the wrong letters in the wrongLetters array
-
   // show user choice, random word, wins/losses/guesses left/wrong letters
   answerText.textContent = answerArray.join(" ");
-  letterGuessedText.textContent = userGuess;
   wrongLettersText.textContent = wrongLetters;
   winsText.textContent = wins;
   lossesText.textContent = losses;
   guessesLeftText.textContent = guessesLeft;
-
-
-
-
 }
 
+// put the correct letter guesses in the blank spaces of answerArray
+
+// put the wrong letters in the wrongLetters array
+
+// add to answer array
 
 // graphic of WOD description when either correctly/incorrectly answered. 
 
@@ -68,3 +86,25 @@ document.onkeyup = function (event) {
 // buttons start and quit onClick
 
 // alert when game is over and out of guesses 
+
+// check if answer is correct
+function correct(guess) {
+  for (var i = 0; i < word.length; i++) {
+    if (guess === word[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function correctUserGuess(word, guess) {
+  console.log("ggg", guess);
+  for (var i = 0; i < word.length; i++) {
+    if (guess === word[i]) {
+      answerArray[i] = word[i]
+      letterLefttoFind--;
+      console.log(answerArray);
+
+    }
+  }
+}
