9d05b7af300ea8531dc71d042a8bf17b9eea39ec
Stacy Holtz <stacyholtz6@gmail.com>
2019-10-15 19:38:37 -0600

    fixed js bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ var guessesLeft = 10;
 // arrays to random word and wrong letter
 var wrongLetters = [];
 var answerArray = [];
+var correctOne = [];
 
 // create variables that reference the html
 var winsText = document.getElementById("wins-text");
@@ -21,6 +22,7 @@ var youWonText = document.getElementById("you-won-text");
 
 // picks a random word from words array
 var word = words[Math.floor(Math.random() * words.length)];
+var newWord = word;
 var letterLefttoFind = word.length;
 
 // calls game function to start the game
@@ -33,14 +35,15 @@ document.onkeyup = function (event) {
   // letterGuessedText.textContent = userGuess;
 
   // decrements the guessesLeft
-  guessesLeft--
-  console.log("guesses left:", guessesLeft)
+  guessesLeft--;
+  console.log("guesses left:", guessesLeft);
+  console.log("correct One:", correctOne);
   // updates the html with the guessesLeft
   guessesLeftText.textContent = guessesLeft;
 
   // correct letter blank spaces gets populated accorndingly
   if (correct(userGuess) == true) {
-    correctUserGuess(word, userGuess)
+    correctUserGuess(word, userGuess);
     answerText.textContent = answerArray.join(" ");
   }
   else {
@@ -54,7 +57,7 @@ document.onkeyup = function (event) {
     word = words[Math.floor(Math.random() * words.length)];
     wins++;
     winsText.textContent = wins;
-    // youWonText.textContent = "Correct!! The word was: " + answerArray.join("");
+    youWonText.textContent = "Correct!!: " + correctOne;
     reset();
 
   }
@@ -63,8 +66,7 @@ document.onkeyup = function (event) {
     word = words[Math.floor(Math.random() * words.length)];
     losses++;
     lossesText.textContent = losses;
-    // youLostText.textContent = "You lost the word was: " + answerArray;
-    // images();
+    youLostText.textContent = "You lost the word was: " + newWord;
     reset();
   }
 
@@ -77,6 +79,7 @@ function gameStart() {
     answerArray[i] = " _ ";
   }
   letterLefttoFind = word.length;
+  correctOne = answerArray;
 
   console.log(answerArray.join(" "));
   console.log("word: " + word);
@@ -103,7 +106,7 @@ function correctUserGuess(word, guess) {
   console.log(guess);
   for (var i = 0; i < word.length; i++) {
     if (guess === word[i]) {
-      answerArray[i] = word[i]
+      answerArray[i] = word[i];
       letterLefttoFind--;
       console.log(answerArray);
 
@@ -123,8 +126,6 @@ function reset() {
 // having issues with showing correct word when lost cant use "word" variable because it gives you the answer not the word that was randomly choosen. If you use answer array it gives you the letters you guessed correctly or all blanks if you didn't get any letters right. 
 
 // sometimes when you guess the right word you don't win..... WHY?????
-
-// not sure how to show YOU WON!!! -- Might be a CSS Thing. Big alert box that says you won... without using "alert in js"
 
 // doesn't show the last letter of the word -- when you complete the word correctly -- 
 
